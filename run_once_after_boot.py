e851a47bd36b9de196ca06ae1ef9ac3202346b03
stefanthurnherr <stefan.thurnherr@gmail.com>
2014-05-19 22:31:53 +0200

    add admin mobile number, send confirmation sms after reboot;

--- run_once_after_boot.py
+++ run_once_after_boot.py
@@ -2,7 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import datetime
+import ConfigParser
+
 from relay import powerswitcher
+from sms import smssender
+
 
 powerswitcher.init_pins()
 power_status = powerswitcher.get_status_string_safe()
@@ -10,4 +14,11 @@ power_status = powerswitcher.get_status_string_safe()
 now = datetime.datetime.now()
 now_text = now.strftime("%Y-%m-%d %H:%M:%S")
 
-print "{0} Successfully initialized pins after boot, power is now {1}.".format(now_text, power_status)
+config = ConfigParser.SafeConfigParser()
+config.read('/home/pi/sms-temperature-control/my.cfg')
+admin_phone_number = config.get('Administrator', 'number')
+
+print "{0} Successfully initialized pins after boot, power is now {1}. Sending confirmation sms to {2}.".format(now_text, power_status, admin_phone_number)
+
+reboot_message = "Hi Admin! Restart completed at {0}, power is now {1}.".format(now_text, power_status)
+smssender.send_sms(reboot_message, admin_phone_number)
