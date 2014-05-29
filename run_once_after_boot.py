b8cef6577623e9a021b0f5eab65a907504afca58
stefanthurnherr <stefan.thurnherr@gmail.com>
2014-05-29 13:43:51 +0200

    add cfg flag to disable/enable admin notification after successful system boot;

--- run_once_after_boot.py
+++ run_once_after_boot.py
@@ -17,8 +17,10 @@ now_text = now.strftime("%Y-%m-%d %H:%M:%S")
 config = ConfigParser.SafeConfigParser()
 config.read('/home/pi/sms-temperature-control/my.cfg')
 admin_phone_number = config.get('Administrator', 'number')
+admin_notify_sms = config.getboolean('Administrator', 'notify_startup_sms')
 
-print "{0} Successfully initialized pins after boot, power is now {1}. Sending confirmation sms to {2}.".format(now_text, power_status, admin_phone_number)
+print "{0} Successfully initialized pins after boot, power is now {1}. Sending confirmation sms to admin ({2})? {3}.".format(now_text, power_status, admin_phone_number, admin_notify_sms)
 
-reboot_message = "Hi Admin! Restart completed at {0}, power is now {1}.".format(now_text, power_status)
-smssender.send_sms(reboot_message, admin_phone_number)
+if admin_notify_sms:
+	reboot_message = "Hi Admin! Restart completed at {0}, power is now {1}.".format(now_text, power_status)
+	smssender.send_sms(reboot_message, admin_phone_number)
