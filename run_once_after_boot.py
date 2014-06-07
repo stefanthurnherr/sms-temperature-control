338f5f2e6108af6e3e5f2e38ed0a78fb12654ed0
stefanthurnherr <stefan.thurnherr@gmail.com>
2014-06-08 00:55:42 +0200

    run usb_modeswitch upon reboot (not yet configurable);

--- run_once_after_boot.py
+++ run_once_after_boot.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import datetime
+import time
 import ConfigParser
+import subprocess
 
 from relay import powerswitcher
 from sms import SmsSender
@@ -16,13 +18,21 @@ now_text = now.strftime("%Y-%m-%d %H:%M:%S")
 
 print "{0} Successfully initialized pins after (re-)boot, power is now {1} ...".format(now_text, power_status)
 
+# FIXME: only run the usb_modeswitch stuff below if required according to lsusb
+
+# run usb_modeswitch to ensure 3G modem is in modem state (and not in storage mode)
+time.sleep(10) # wait until modem is connected
+return_code = subprocess.call("/usr/sbin/usb_modeswitch -c /etc/usb_modeswitch.conf", shell=True)
+print "{0}    Ran usb_modeswitch, got return code {1}".format(now_text, return_code)
+if int(return_code) == 0:
+    time.sleep(60) # sleep a while after disconnect to give modem time to (re-)connect
+
 config = ConfigParser.SafeConfigParser()
 config.read('/home/pi/sms-temperature-control/my.cfg')
 admin_phone_number = config.get('Administrator', 'number')
 admin_notify_sms = config.getboolean('Administrator', 'notify_startup_sms')
 
-print "{0}   Sending confirmation sms to admin ({1})? {2}.".format(now_text, admin_phone_number, admin_notify_sms)
-
+print "{0}    Sending confirmation sms to admin ({1})? {2}.".format(now_text, admin_phone_number, admin_notify_sms)
 if admin_notify_sms:
     gammu_config_file = config.get('Phone', 'gammu_config_file')
     gammu_config_section = config.get('Phone', 'gammu_config_section')
