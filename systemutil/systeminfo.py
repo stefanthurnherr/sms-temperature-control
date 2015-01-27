4935c867eb095cb6a274e472cfcbf39b82f3e8c7
stefanthurnherr <stefan.thurnherr+github@gmail.com>
2015-01-27 22:39:25 +0100

    error/stderr handling for subprocesses;

--- systemutil/systeminfo.py
+++ systemutil/systeminfo.py
@@ -1,28 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import commands
+import os
 from subprocess import Popen, PIPE
 
 
 def get_rpi_serial_number():
-    cpuInfoLines = commands.getoutput("cat /proc/cpuinfo").split("\n")
-    #cpuInfoLines = ["hello", "Serial      : 000000065gh7890 ", "world"]
-    cpuInfoLines = filter(lambda s: "Serial  " in s, cpuInfoLines)
-    return cpuInfoLines[0].split(":")[1].lstrip(' 0')
+    with open(os.devnull, "w") as fnull:
+        cpuInfoProc = Popen(['cat', '/proc/cpuinfo'], stdout = PIPE, stderr = fnull)
+        (stdout, _) = cpuInfoProc.communicate()
+        if stdout:
+            cpuInfoLines = filter(lambda s: "Serial  " in s, stdout.split("\n"))
+            return cpuInfoLines[0].split(":")[1].lstrip(' 0')
+        else:
+            return None
 
 
 def get_inet_address():
-    localIpAddress = commands.getoutput("/sbin/ifconfig").split("\n")[1] 
-    if 'inet' in localIpAddress:
-        return localIpAddress.split()[1][5:]
-    else:
-        return None
+    with open(os.devnull, "w") as fnull:
+        ifConfigProc = Popen('/sbin/ifconfig', stdout = PIPE, stderr = fnull)
+        (stdout, _) = ifConfigProc.communicate()
+        localIpAddress = stdout.strip().split("\n")[1]
+        if 'inet' in localIpAddress:
+            return localIpAddress.split()[1][5:]
+        else:
+            return None
+
 
 def get_git_revision():
-    gitproc = Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout = PIPE)
-    (stdout, _) = gitproc.communicate()
-    return stdout.strip()
+    with open(os.devnull, "w") as fnull:
+        gitproc = Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout = PIPE, stderr = fnull)
+        (stdout, _) = gitproc.communicate()
+        if stdout:    
+            return stdout.strip()
+        else:
+            return None
 
 
 if __name__ == "__main__":
