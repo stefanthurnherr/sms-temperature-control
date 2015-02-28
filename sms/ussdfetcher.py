0c872227e63ee862cd63ac6381f6e180e9c7e6b4
Stefan Thurnherr <stefan.thurnherr@gmail.com>
2015-03-01 00:35:15 +0100

    fix method names;

--- sms/ussdfetcher.py
+++ sms/ussdfetcher.py
@@ -22,13 +22,13 @@ class UssdFetcher(object):
         return None
 
  
-    def convert_reply_raw_to_string(self, reply_raw):
-        reply_string = reply_raw.decode('hex').decode('latin1')
-        return reply_string
+    def convert_reply_raw_to_unicode(self, reply_raw):
+        reply_unicode = reply_raw.decode('hex').decode('latin1')
+        return reply_unicode
 
 
-    def convert_reply_string_to_raw(self, reply_string):
-        reply_raw = reply_string.encode('latin1').encode('hex')
+    def convert_reply_unicode_to_raw(self, reply_unicode):
+        reply_raw = reply_unicode.encode('latin1').encode('hex')
         return reply_raw
 
 
@@ -45,11 +45,11 @@ if __name__ == "__main__":
     print "completed gammu ussd call, got:"
     print reply_raw
 
-    print "type is: {0}.".format(type(reply_raw))
-    reply_string = ussd_fetcher.convert_reply_raw_to_string(reply_raw)
-    print "type is: {0}.".format(type(reply_string))
+    print "type (raw) is: {0}.".format(type(reply_raw))
+    reply_unicode = ussd_fetcher.convert_reply_raw_to_unicode(reply_raw)
+    print "type (unicode) is: {0}.".format(type(reply_unicode))
     print "ASCII-decoded reply (with non-ASCII characters replaced) is:"
-    print reply_string.encode('ascii', 'replace') # remember: python default encoding is 'ascii'
+    print reply_unicode.encode('ascii', 'replace') # remember: python default encoding is 'ascii'
 
     #reply_raw = "0053004D0053003A006100200030003500470042002000740069006C006C00200031003300330020007300E50020006B0061006E00200064007500200073007500720066006100200030002C003500470042002000690020003300300020006400610067006100720020006600F6007200200065006E00640061007300740020003300300020006B0072002E000A00530041004C0044004F003A002000340036002C003300380020006B0072000A00500072006900730070006C0061006E003A0020005300740061006E0064006100720064"
 
